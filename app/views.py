860feff6151b33c95a233e039946042f3e696025
YanagiNaoto1017 <n.yanagi1017@gmail.com>
2024-11-08 17:24:57 +0900

    実行できるようにするため変更

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
 
-from django.views.generic.base import TemplateView, ListView
+from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 from .forms import SignUpForm
-from .forms import AdminListForm
 # from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Admin
 
@@ -22,10 +22,11 @@ class SignupView(TemplateView):
     template_name = 'admin_signup.html'
     success_url = reverse_lazy("app:index")
 
-class ListView(ListView):
-    """管理者一覧"""
-
-    model = Admin
-    form_class = AdminListForm
-    template_name = 'admin_list.html'
-    success_url = reverse_lazy("app:index")
+    def form_valid(self, form):
+        # ユーザー作成後にそのままログイン状態にする設定
+        response = super().form_valid(form)
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(email=email, password=password)
+        login(self.request, user)
+        return response
